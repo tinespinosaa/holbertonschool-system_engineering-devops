d50093cf91bd6e56c400b0856224e6004525324e
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-08-12 23:52:03 -0500

    task1 Using Puppet, install flask from pip3..

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# Install a package
-package { 'puppet_lint':
-    ensure   => '2.5.0',
-    name     => 'puppet-lint',
-    provider => 'gem',
-    source   => 'http://rubygems.org',
+# This manifest install flask from pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

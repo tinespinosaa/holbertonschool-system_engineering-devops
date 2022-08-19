74a4815c8fb549d996c913dbfd4ca166b5d58371
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-08-18 21:53:20 -0500

    task6 Install Nginx web server (w/ Puppet).

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,5 +4,6 @@ command => 'sudo apt-get -y update ;\
            sudo apt install -y nginx ;\
            sudo echo 'Hello World' | sudo tee /var/www/html/index.nginx-debian.html ;\
            new_string="a location /redirect_me\n \t{ \n\t rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; \n \t}" ;\
+	   sudo sed -i "/server_name _;/$new_string" /etc/nginx/sites-available/default ;\
            sudo service nginx restart',
 }

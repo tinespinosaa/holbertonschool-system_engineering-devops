5d94ffebfbe485ec85f95527e0040606a07279a3
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-10-06 22:12:35 -0500

    new file

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Debugging file to our web server
+exec { 'file limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}

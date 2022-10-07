bfe66cf49cc182ae65811dbcaab6d172d256a0dd
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-10-06 22:18:09 -0500

    new files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
-# Debugging file to our web server
-exec { 'file limit':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+# fix too many files error
+exec { 'config nginx':
+  command  => 'sed -i -e "s/-n 15/-n 4096/" /etc/default/nginx; service nginx restart',
   provider => shell,
 }
